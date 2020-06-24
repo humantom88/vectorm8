1e54cea4dbd966e3d6f4a63a2783b7385e7b8dd3
RandyMarsh1993 <dturanin@mail.ru>
2020-06-24 16:32:32 +0300

    удалил лишнее. Глобально установил axios. /signin посылает данные на /login

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 const MongoClient = require("mongodb").MongoClient;
 
@@ -14,85 +15,42 @@ const MongoClient = require("mongodb").MongoClient;
     
 });*/
 
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   //const data = await collection.find({}).toArray();
   //console.log(data)
   res.render('index', {
-      ivan: 'Ivan',
-      //news: data
+    ivan: 'Ivan',
+    //news: data
   });
 });
 
-router.get('/dashboard', function(req, res) {
-    res.render('ivan')
-})
+router.get('/signin', function (req, res) {
+  res.render('signin');
+});
 
-router.get('/contacts', function(req, res) {
-    res.render('contacts', {
-        number: '8999666',
-    });
-})
+router.get('/contacts', function (req, res) {
+  res.render('contacts');
+});
 
-router.get('/about', function(req, res) {
-    res.render('about', {
-        title: 'about'
-    })
-})
+router.get('/about', function (req, res) {
+  res.render('about', {
+    title: 'about'
+  })
+});
 
-router.get('/vue', function(req, res) {
-    res.send(`
-      <html>
-        <head>
-          <link rel='stylesheet' href='/stylesheets/style.css' />
-        </head>
-        <body>
-          <div id="app">
-            {{ message }}
-          </div>
-          <div id="app-2">
-            <span v-bind:title="message">
-              Hover your mouse over me for a few seconds
-              to see my dynamically bound title!
-            </span>
-          </div>
-          <script src="https://cdn.jsdelivr.net/npm/vue"></script>
-          <script>
-          var app = new Vue({
-            el: '#app',
-            data: {
-              message: 'Hello Vue!'
-            }
-          })
-
-          var app2 = new Vue({
-            el: '#app-2',
-            data: {
-              message: 'You loaded this page on ' + new Date().toLocaleString()
-            }
-          })
-          </script>
-        </body>
-      </html>`)
-})
-
-router.post('/registration', function(req, res) {
-    const firstname = req.body.firstname
-    const lastname = req.body.lastname
-    const middlename = req.body.middlename
-    const fruits = req.body.fruits
-
-    res.send(`
-      <html>
-        <head>
-          <link rel='stylesheet' href='/stylesheets/style.css' />
-        </head>
-        <body>
-          <h1>${firstname}</h1>
-          <h1>${lastname}</h1>
-          <h1>${middlename}</h1>
-          <h1>${fruits}</h1>
-        </body>
-      </html>`)
-})
+router.post('/login', function (req, res) {
+  let login = req.body.login;
+  let password = req.body.password;
+  res.send(`
+  <html>
+    <head>
+      <link rel='stylesheet' href='/stylesheets/style.css' />
+    </head>
+    <body>
+      <h2>login: ${login}</h2>
+      <h2>password: ${password}</h2>
+    </body>
+  </html>`);
+});
 
 module.exports = router;
